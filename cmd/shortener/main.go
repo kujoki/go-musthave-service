1beb9673c5b8e3fa78d9b7fdeaa0a7460fb66522
Kate <zhdkat.zxc@gmail.com>
2025-08-10 16:26:33 +0300

    Six iter

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,36 +2,49 @@ package main
 
 import (
 	"net/http"
-	"log"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 	"github.com/kujoki/go-musthave-service/internal/handler"
 	"github.com/kujoki/go-musthave-service/internal/config"
 	"github.com/kujoki/go-musthave-service/internal/service"
+	l "github.com/kujoki/go-musthave-service/internal/logger"
 )
+
+var sugar zap.SugaredLogger
 
 func main() {
 	cfg := config.ParseFlags()
     
     if err := run(cfg); err != nil {
-        log.Fatalf("Server failed to start: %v", err)
+		sugar.Fatalw(err.Error(), "event", "start server")
     }
 }
 
 
 func run(cfg *config.Config) error {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+        panic(err)
+    }
+    defer logger.Sync()
+	sugar = *logger.Sugar()
+
 	s := service.NewService()
 
 	r := chi.NewRouter()
 
-	log.Println("Running server on", cfg.RunAddr)
+	postHandler := handler.WrapperPostSlash(cfg.BaseURL, s)
+	getHandler := handler.WrapperGetSlashURL(s)
 
-	r.Post("/", handler.WrapperPostSlash(cfg.BaseURL, s))
-	r.Get("/{ID}", handler.WrapperGetSlashURL(s))
+	r.Post("/",  l.WithLogging(sugar, postHandler))
+	r.Get("/{ID}", l.WithLogging(sugar, getHandler))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(`this request are not allowed!`))
 	})
+
+	sugar.Infow("Running server", "address", cfg.RunAddr)
 
 	return http.ListenAndServe(cfg.RunAddr, r)
 }
